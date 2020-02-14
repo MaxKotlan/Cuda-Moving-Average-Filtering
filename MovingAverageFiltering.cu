24c1cf57fdc96fe950f1d0d94ede38e26a299dd2
max <max.maxkot@gmail.com>
2020-02-14 17:49:21 -0500

    Cuda allocation

--- MovingAverageFiltering.cu
+++ MovingAverageFiltering.cu
@@ -7,25 +7,55 @@ struct Startup{
     int random_range = 10;
 } startup;
 
+/*
+ Found on the stack overflow:  https://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
+ Throws errors if cuda command doesn't return Success
+*/
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
 struct DataSet{
-    int* values;
+    float* values;
     int  size;
 };
+
+inline int sizeOfDataSet(DataSet data){ return sizeof(float)*data.size; }
 
 DataSet generateRandomDataSet(int size){
     DataSet data;
     data.size = size;
-    data.values = (int*)malloc(sizeof(int)*data.size);
+    data.values = (float*)malloc(sizeof(float)*data.size);
 
     for (int i = 0; i < data.size; i++)
-        data.values[i] = rand()%startup.random_range;
+        data.values[i] = (float)(rand()%startup.random_range);
 
     return data;
 }
 
+DataSet cudaCalculateSMA(DataSet input, int sample_size){
+    DataSet host_result = {(float*)malloc(sizeof(float)*(input.size-sample_size)), input.size-sample_size};
+
+    float* device_input, device_result;
+
+    gpuErrchk(cudaMalloc((void **)&device_input,  sizeOfDataSet(input) ));
+    gpuErrchk(cudaMalloc((void **)&device_result, sizeOfDataSet(host_result) ));
+
+    gpuErrchk(cudaMemcpy(device_input, input.values, sizeOfDataSet(input) , cudaMemcpyHostToDevice));
+
+
+    return host_result;
+}
+
 void printDataSet(DataSet data){
     for (int i = 0; i < data.size; i++)
-        printf("%d ", data.values[i]);
+        printf("%.0f ", data.values[i]);
     printf("\n");
 }
 
